00c2733202eb9076dec22b3ed33b7a3143d629fb
GasinskiN <gasinskinorman@gmail.com>
2023-04-13 23:02:33 +0200

    Update declarations

--- src/declarations/dbank/dbank.did.d.ts
+++ src/declarations/dbank/dbank.did.d.ts
@@ -1,2 +1,5 @@
 import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {}
+export interface _SERVICE {
+  'addValue' : (arg_0: bigint) => Promise<undefined>,
+  'withdrawValue' : (arg_0: bigint) => Promise<undefined>,
+}
--- src/declarations/dbank/dbank.did.js
+++ src/declarations/dbank/dbank.did.js
@@ -1,2 +1,7 @@
-export const idlFactory = ({ IDL }) => { return IDL.Service({}); };
+export const idlFactory = ({ IDL }) => {
+  return IDL.Service({
+    'addValue' : IDL.Func([IDL.Nat], [], ['oneway']),
+    'withdrawValue' : IDL.Func([IDL.Nat], [], ['oneway']),
+  });
+};
 export const init = ({ IDL }) => { return []; };
